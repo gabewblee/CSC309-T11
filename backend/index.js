f48a06b04186999d888ee95ff6a4b3d223a767eb
Gabriel Lee <gabe.wblee@gmail.com>
2025-11-23 22:28:21 -0500

    Removing index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,7 @@
-import express from "express";
-import routes from "./routes.js";
-import cors from "cors";
+// import express from "express";
+// import routes from "./routes.js";
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
-const app = express();
+// const router = express.Router();
 
-// TODO: complete me (CORS)
-app.use(cors({
-    origin: FRONTEND_URL,
-    credentials: true
-}));
-
-app.use(express.json());
-app.use('', routes);
-
-export default app;
+// router.use("", routes);
+// export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config();
-const port = process.env.PORT || 3000;
-import app from "./index.js";
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
 
+dotenv.config();
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const app = express();
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}));
+app.use(express.json());
+app.use(routes);
+
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
